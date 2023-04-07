e3b3742d1bbcacb03c96b6cae5b1e6c48cb64a29
beniroquai <bene.d@gmx.de>
2023-04-07 23:11:52 +0200

    Adapting API to ImSwitch

--- src/app/service/navigation.service.ts
+++ src/app/service/navigation.service.ts
@@ -9,6 +9,28 @@ export class NavigationService {
     }
 
     public setSpeed(leftForward: boolean, pwmLeft: number, rightForward: boolean, pwmRight: number): Promise<string> {
+
+        let axis = "X";
+        let dist = 0;
+        // convert left/right to x/y and send to positioner
+        if (leftForward) {
+            axis = "X";
+            dist = pwmLeft;
+        }
+        else if (rightForward) {
+            axis = "Y";
+            dist = pwmRight;
+        }
+        else {
+            axis = "XY";
+            dist = pwmLeft;
+        }
+
+        // formulate url with axis/dist 
+        // http://localhost:8000/PositionerController/movePositioner?positionerName=ESP32Stage&axis=X&dist=10000&isAbsolute=false
+        return this.httpService.get<string>(`:8000/PositionerController/movePositioner?positionerName=ESP32Stage&axis=${axis}&dist=${dist}&isAbsolute=false`);
+        
+
         return this.httpService.get<string>(`/navi?dl=${leftForward ? 1 : 0}&sl=${pwmLeft}&dr=${rightForward ? 1 : 0}&sr=${pwmRight}`);
     }
 
